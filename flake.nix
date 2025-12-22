aac49fc154a5d9d5abfc55226735a53fee525174
Bautista <b.alvarez.reynolds@usal.edu.ar>
2025-12-23 01:29:11 +0900

    modified nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Hyprland on Nixos";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
