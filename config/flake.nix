33c16f0bba37c69a748d1a30fb029ce40cfd2872
Bautista <b.alvarez.reynolds@usal.edu.ar>
2025-12-22 20:51:35 +0900

    initial commit

---
+++ config/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Hyprland on Nixos";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
+    nixosConfigurations.bau-pc = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.bau = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
+}
