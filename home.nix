ea7eeddebbf3c28a848dd42b9f22abcffd8a3a0f
Bautista <b.alvarez.reynolds@usal.edu.ar>
2025-12-22 23:16:12 +0900

    changed structure, now includes config for waybar

--- home.nix
+++ home.nix
@@ -9,5 +9,13 @@
     shellAliases = {
       nrs = "nixos-rebuild switch --flake /etc/nixos#bau-pc";
     };
+    initExtra = ''
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+    '';
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+          exec hyprland
+      fi
+    '';
   };
 }
