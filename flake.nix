f095149729d646b85f16f358ad85e99f963c4ec0
Bautista <b.alvarez.reynolds@usal.edu.ar>
2025-12-25 07:30:33 +0900

    various changes, started moving packages to home.nix instead of having everything on configuration.nix, renamed from config to dotfiles (can easily tab in terminal to configuration.nix now)

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "Hyprland on Nixos";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- home.nix
+++ home.nix
@@ -1,8 +1,37 @@
 { config, pkgs, ... }:
 {
   home.username = "bau";
-  home.stateVersion = "25.05";
-  programs.git.enable = true;
+  home.stateVersion = "25.11";
+
+  # Packages moved from configuration.nix
+  home.packages = with pkgs; [
+    firefox
+    vscode
+    anki-bin
+    kitty
+    bat
+    btop
+    mpv
+    neofetch
+    wl-clipboard
+    tree
+    hyprpaper
+  ];
+
+  programs.git = {
+    enable = true;
+    # Consider adding user config here:
+    # userName = "Your Name";
+    # userEmail = "your.email@example.com";
+  };
+  
+  programs.rofi.enable = true;
+  
+  programs.pywal = {
+    enable = true;
+    package = pkgs.pywal16;
+  };
+  
   programs.bash = {
     enable = true;
     shellAliases = {
@@ -10,15 +39,19 @@
     };
     initExtra = ''
       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
-    '';
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-          exec hyprland
-      fi
+      ns() {
+        nix shell ''${@/#/nixpkgs#}
+      }
     '';
   };
 
   xdg.configFile = {
-    waybar.source = ./config/waybar;
+    waybar.source = ./dotfiles/waybar;
+    kitty.source = ./dotfiles/kitty;
+    rofi.source = ./dotfiles/rofi;
+
   };
+
+  # Let Home Manager manage itself
+  programs.home-manager.enable = true;
 }
